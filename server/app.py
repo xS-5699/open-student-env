9a7ac08b55412e06d1e1880716ad5ac4d0b21289
xS-5699 <ishaanmaths@gmail.com>
2026-04-08 22:40:09 +0530

    Fix main entrypoint

--- server/app.py
+++ server/app.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
 from env.environment import StudentEnv
+import uvicorn
 
 app = FastAPI()
 env = StudentEnv()
 
+
 @app.post("/reset")
 def reset():
     return {"state": env.reset("medium")}
+
 
 @app.post("/step")
 def step(action: dict):
@@ -18,9 +21,15 @@ def step(action: dict):
         "info": info
     }
 
+
 @app.get("/state")
 def state():
     return env.state()
 
+
 def main():
-    return app
+    uvicorn.run(app, host="0.0.0.0", port=7860)
+
+
+if __name__ == "__main__":
+    main()

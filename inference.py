707ccd6a684bd1eea0eb21c38be4fdf215c67337
xS-5699 <ishaanmaths@gmail.com>
2026-04-12 22:36:37 +0530

    Changes for Phase 2

--- inference.py
+++ inference.py
@@ -11,7 +11,7 @@ load_dotenv()
 
 print("[START]")
 
-API_KEY = os.getenv("OPENAI_API_KEY")
+API_KEY = os.getenv("API_KEY") or os.getenv("OPENAI_API_KEY")
 BASE_URL = os.getenv("API_BASE_URL")
 MODEL_NAME = os.getenv("MODEL_NAME")
 
